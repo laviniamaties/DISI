44ba523739bd4c1ab9f5b94c3b5ca38a94e917d0
Adrian Bordeianu <adrian.bordeianu@accesa.eu>
2019-04-08 18:00:46 +0300

    Routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import LoginForm from './Components/LoginForm'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage/Login.page';
+import TeacherHomePage from './Pages/TeacherHomePage/teacher-home.page';
+import StudentHomePage from './Pages/StudentHomePage/student-home.page';
+import SecretaryHomePage from './Pages/SecretaryHomePage/secretary-home.page';
 
 class App extends Component {
-  public render(): any { 
+  public render(): any {
     return(
-      <div className="app">
-        <LoginPage />
-      </div>
+      <Router>
+        <Route path="/" exact component={LoginPage} />
+        <Route path="/teacher/" component={TeacherHomePage} />
+        <Route path="/student/" component={StudentHomePage} />
+        <Route path="/secretary/" component={SecretaryHomePage} />
+      </Router>
     )
   }
 }
