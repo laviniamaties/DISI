53744637b911ead81566b747f38cc5f17e56b2e7
Your Name <you@example.com>
2019-04-14 11:47:30 +0300

    add functionality to switch between login / register form

--- client/src/Components/login-form.tsx
+++ client/src/Components/login-form.tsx
@@ -7,12 +7,14 @@ class LoginForm extends Component {
         email: '',
         password: '',
         role: 'Student',
-        open: false
+        open: false, 
+        isRegisterForm: false
     }
+
+    // private isRegisterForm = false;
 
     private handleSubmit = (e: any) => {
         e.preventDefault();
-
         console.log(this.state)
     }
 
@@ -28,12 +30,18 @@ class LoginForm extends Component {
         })
     }
 
+    private togglRegisterState(e: any){
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({isRegisterForm: !this.state.isRegisterForm});
+    }
+
     public render(): any {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col text-center">
-                        <h1>Login</h1>
+                        <h1>{this.state.isRegisterForm ? "Login" : "Register"}</h1>
                         <form onSubmit={this.handleSubmit}>
                             <div className='login-form'>
                                 <div>Email</div>
@@ -53,17 +61,22 @@ class LoginForm extends Component {
                                     onChange={this.handleChange}
                                     datatype={'password'}
                                 />
-                                <div>Type</div>
+                                {
+                                    !this.state.isRegisterForm ?
+                                    <div>
+                                        <div>Type</div>
+                                        <div>
+                                            <select value={this.state.role} onChange={this.selectRole} className="form-control">
+                                                <option>Student</option>
+                                                <option>Secretary</option>
+                                                <option>Teacher</option>
+                                            </select>
+                                        </div>
+                                    </div> : <div></div>
+                                }
+                                <button className="btn btn-primary login-btn">{this.state.isRegisterForm ? "Login" : "Register"}</button>
                                 <div>
-                                    <select value={this.state.role} onChange={this.selectRole} className="form-control">
-                                        <option>Student</option>
-                                        <option>Secretary</option>
-                                        <option>Teacher</option>
-                                    </select>
-                                </div>
-                                <button className="btn btn-primary login-btn">Login</button>
-                                <div>
-                                    <a href="#">Create an Account</a>
+                                    <a href="#" onClick={(e) => this.togglRegisterState(e)}>{this.state.isRegisterForm ? "Create an Account" : "Already have an account? Sign in"}</a>  
                                 </div>
                             </div>
                         </form>
